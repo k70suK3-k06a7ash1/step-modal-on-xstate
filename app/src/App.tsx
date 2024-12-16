e94cfe60408688c97f586646a7b4ed1459b306a2
2814109 <2814109k@gmail.com>
2024-12-16 13:35:33 +0900

    chore

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,12 @@
+import { Button } from "@/components/ui/button";
 import "./App.css";
 
 function App() {
-  return <>Hello</>;
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
 }
 
 export default App;
